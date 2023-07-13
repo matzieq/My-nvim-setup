cb7d213f9895af31c87a9f04cd55da9300c61cf6
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2023-07-13 09:54:59 +0200

    Ignore node modules and .git

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,11 +5,16 @@ telescope.setup {
         find_files = {
             hidden = true
         }
+    },
+    defaults = {
+        file_ignore_patterns = {
+            "node_modules",
+            ".git"
+        }
     }
 }
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
